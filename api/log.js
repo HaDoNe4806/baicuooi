97134fbed91fd54952470a3ed33225d938ad605e
HaDoNe4806 <hado040806@gmail.com>
2025-05-17 16:18:32 +0700

    test5

--- api/log.js
+++ api/log.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const flag = req.query.flag || "no-flag";
-  console.log("💡 FLAG RECEIVED:", flag); // bắt buộc phải có
+  console.log("💡 FLAG RECEIVED:", flag); // log để xem trong Vercel
 
-  res.status(200).end(); // trả response OK
+  res.status(200).send("Logged!");
 }
