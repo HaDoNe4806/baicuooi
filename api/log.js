15c6f831f5b709e36a929723c6afcb7915985df2
HaDoNe4806 <hado040806@gmail.com>
2025-05-17 15:58:00 +0700

    test4

--- api/log.js
+++ api/log.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
-  const flag = req.query.flag;
-  console.log("FLAG:", flag);
-  res.status(200).send("ok");
+  const flag = req.query.flag || "no-flag";
+  console.log("💡 FLAG RECEIVED:", flag); // bắt buộc phải có
+
+  res.status(200).end(); // trả response OK
 }
